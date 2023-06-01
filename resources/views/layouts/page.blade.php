faec476fc7a8bbb6a141472728ccbca82e32541b
namif017 <80682670+namif017@users.noreply.github.com>
2023-06-01 19:21:55 +0900

    jqueryを導入

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -8,6 +8,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('css')
     <title>@yield('title')</title>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container">
